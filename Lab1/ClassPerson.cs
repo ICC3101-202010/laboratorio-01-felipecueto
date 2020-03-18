24d6e79bc2ecb78ae34607cfd05f8e396f703715
Felipe <felipe@MacBook-Pro-de-Felipe-2.local>
2020-03-18 14:11:01 -0300

    parte1

---
+++ Lab1/ClassPerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Lab1
+{
+    public class Person
+    {
+        //Atributos
+        public String Name;
+        public String LastName;
+
+        //Comportamiento
+        public int Lanzar()
+        {
+            Random ran = new Random();
+            int num = ran.Next(0, 3);
+            return num;
+
+        }
+
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Person call = new Person();
+            int numero = call.Lanzar();
+            Console.WriteLine(numero);
+
+            
+        }
+    }
+
+}
